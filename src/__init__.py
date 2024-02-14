963c1d5f0cedfe2b1d462a712613afb28436f51d
eoussama <oussama.essamadi@gmail.com>
2024-02-14 13:38:11 +0100

    feat: fixes and tweaks, added type

--- src/__init__.py
+++ src/__init__.py
@@ -30,7 +30,8 @@ def scrape_reviews(user_id):
             
             review_data['title'] = review.find('a', class_='title').text
             review_data['url'] = review.find('a', class_='title')['href']
-            review_data['id'] = get_id(review.find('a', class_='title')['href'])
+            review_data['type'] = get_type(review_data['url'])
+            review_data['id'] = get_id(review_data['type'], review_data['url'])
             review_data['date'] = review.find('div', class_='update_at').text
             review_data['time'] = review.find('div', class_='update_at')['title']
             review_data['recommendation'] = review.find('div', class_='tag').text
@@ -70,14 +71,22 @@ def slugify(value, allow_unicode=False):
     value = re.sub(r'[^\w\s-]', '', value.lower())
     return re.sub(r'[-\s]+', '-', value).strip('-_')
 
-def get_id(url):
-    match = re.search(r'/anime/(\d+)/', url)
+def get_id(review_type, url):
+    match = re.search(rf'/{review_type}/(\d+)/', url)
 
     if match:
         id = match.group(1)
         return int(id)
     else:
         return -1
+
+def get_type(url):
+    parts = [part for part in url.split('/') if part]
+
+    if len(parts) >= 4:
+        return parts[2]
+    else:
+        return 'unknown'
 
 if __name__ == '__main__':
   user_id = sys.argv[1]
